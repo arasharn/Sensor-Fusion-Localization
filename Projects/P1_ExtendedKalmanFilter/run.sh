1f2931a51895cc88eaaefd46952acedb0e6fdb56
SaynaEbrahimi <sayna@berkeley.edu>
2017-03-28 19:01:40 -0700

    adding figures to repo

--- Projects/P1_ExtendedKalmanFilter/run.sh
+++ Projects/P1_ExtendedKalmanFilter/run.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 clear
 
-echo "Changing the directory to the where src folder exists ..."
 cd /Users/sayna/Dropbox/Udacity/Term2_SDCND/Projects/P1_ExtendedKalmanFilter
 mkdir build
 cd build
 cmake .. && make
-./ExtendedKF ../data/sample-laser-radar-measurement-data-1.txt ../data/sample-laser-radar-measurement-data-1.out
+# ./ExtendedKF ../data/sample-laser-radar-measurement-data-1.txt ../data/sample-laser-radar-measurement-data-1.out
 ./ExtendedKF ../data/sample-laser-radar-measurement-data-2.txt ../data/sample-laser-radar-measurement-data-2.out
